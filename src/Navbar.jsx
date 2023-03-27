8cbd58b01c390de6fc77320df10720241ac1de2e
Brian4755 <brian.mccune1@yahoo.com>
2023-03-27 15:34:57 -0500

    navbar styling

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -4,9 +4,11 @@ const Navbar = () => {
   return ( 
     <nav>
       <Link to='/'><img className="imageIcon" src="./homepagebutton.png" alt="Jack's Barbershop"/></Link>
+      <div className="navLinks">
       <Link className="location" to='/location'>Location</Link>
       <Link className="about" to='/about'>About</Link>
       <Link className="contact" to='/contact'>Contact</Link>
+      </div>
     </nav>
    )
 }
