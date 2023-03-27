76236fbc9c3beba8cd5560fc71eec7e1939591e4
Brian4755 <brian.mccune1@yahoo.com>
2023-03-27 05:58:01 -0500

    image import for homepage route

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 const Navbar = () => {
   return ( 
     <>
-      <Link to='/'>Home</Link>
+      <Link to='/'><img src="./homepagebutton.png" alt="Jack's Barbershop"/></Link>
       <Link to='/location'>Location</Link>
       <Link to='/about'>About</Link>
       <Link to='/contact'>Contact</Link>
