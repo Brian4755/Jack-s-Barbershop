96c8418f515123f6ad751e09ecea337a89180664
Brian4755 <brian.mccune1@yahoo.com>
2023-03-27 05:32:16 -0500

    base navbar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './Navbar'
 
 function App() {
   return (
     <>
+    <Navbar />
+    <Routes>
+      {/* <Route path='' element={}/>
+      <Route path='' element={}/>
+      <Route path='' element={}/>
+      <Route path='' element={}/>
+      <Route path='' element={}/> */}
+    </Routes >
     </>
   )
 }
