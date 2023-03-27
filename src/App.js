2a1141f05b50c09171b750389db5cab6884f510f
Brian4755 <brian.mccune1@yahoo.com>
2023-03-27 05:43:14 -0500

    rename homepage to home and add button link/path

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css'
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './Navbar'
+import Home from './Home';
 
 function App() {
   return (
     <>
     <Navbar />
     <Routes>
+      <Route path='/' element={<Home />}/>
       {/* <Route path='' element={}/>
-      <Route path='' element={}/>
       <Route path='' element={}/>
       <Route path='' element={}/>
       <Route path='' element={}/> */}
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom"
 const Navbar = () => {
   return ( 
     <>
+      <Link to='/'>Home</Link>
     </>
    )
 }
