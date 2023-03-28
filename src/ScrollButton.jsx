82dddca0557145ee4f9bfecbe82f6539de19dc9c
Brian4755 <brian.mccune1@yahoo.com>
2023-03-28 18:39:26 -0500

    scrolltop visibility functionality

--- src/ScrollButton.jsx
+++ src/ScrollButton.jsx
@@ -1,8 +1,23 @@
 import './ScrollButton.css'
+import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
 
 const ScrollButton = () => {
+  const [visible, setVisible] = useState(false)
+
+  function handleVisibleScroll() {
+    const scrolled = document.documentElement.scrollTop
+    if (scrolled > 1800){
+      setVisible(true)
+    } 
+    else if (scrolled <= 1800){
+      setVisible(false)
+    }
+  }
+
+  window.addEventListener('scroll', handleVisibleScroll)
+
   function handleScrollTop() {
     window.scrollTo({
       top: 0,
@@ -12,7 +27,10 @@ const ScrollButton = () => {
 
   return ( 
     <>
-    <button id='scrollBtn' className="fab fa-react fa-3x" onClick={handleScrollTop}><FontAwesomeIcon icon={faArrowUp} /></button>
+      {
+        visible === true &&
+        <button id='scrollBtn' className="fab fa-react fa-3x" onClick={handleScrollTop}><FontAwesomeIcon icon={faArrowUp} /></button>
+      }
     </>
    )
 }
