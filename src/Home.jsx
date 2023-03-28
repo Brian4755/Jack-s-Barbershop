eb8e5b7879c39a4d71965986bcfd13a05ebe5c0f
Brian4755 <brian.mccune1@yahoo.com>
2023-03-28 00:04:31 -0500

    image homepage resizing

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,8 +3,11 @@ import './Home.css'
 const Home = () => {
   return ( 
     <>
-    <h1>Jack's Barbershop</h1>
-    <img className='barbershop' src="./barbershop.jpg" alt="Barbershop" />
+    <div className='homeImages'>
+    <img className='insideshop' src="./insidebarbershop.jpg" alt="Inside Area" />
+    {/* <img className='barbershop' src="./barbershop.jpg" alt="Barbershop" />
+    <img className='barberself' src="./barberself.jpg" alt="Barber" /> */}
+    </div>
     </>
    )
 }
