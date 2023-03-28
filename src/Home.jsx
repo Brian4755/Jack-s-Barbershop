c6392af5f56473f6addb00145505912a1d572e14
Brian4755 <brian.mccune1@yahoo.com>
2023-03-28 10:08:42 -0500

    hours of operation component

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,6 @@
 import Carousel from './Carousel'
 import ExampleCuts from './ExampleCuts'
+import Hours from './Hours'
 import './Home.css'
 
 const Home = () => {
@@ -24,39 +25,7 @@ const Home = () => {
       </div>
       <img className='barberself' src="./barberself.jpg" alt="Lead Barber" />
       <img className='barberStation' src="./barberstations.jpg" alt="Barber Stations" />
-      <div>
-        <h2>Hours of Operation:</h2>
-        <table>
-          <tr>
-            <td>Monday</td>
-            <td>Closed</td>
-          </tr>
-          <tr>
-            <td>Tuesday</td>
-            <td>9:00AM - 7:00PM</td>
-          </tr>
-          <tr>
-            <td>Wednesday</td>
-            <td>9:00AM - 6:00PM</td>
-          </tr>
-          <tr>
-            <td>Thursday</td>
-            <td>9:00AM - 7:00PM</td>
-          </tr>
-          <tr>
-            <td>Friday</td>
-            <td>9:00AM - 7:00PM</td>
-          </tr>
-          <tr>
-            <td>Saturday</td>
-            <td>9:00AM - 7:00PM</td>
-          </tr>
-          <tr>
-            <td>Sunday</td>
-            <td>Closed</td>
-          </tr>
-        </table>
-      </div>
+      <Hours />
     </div>
     <div className='facebookSecBackground'>
       <div className='facebookSection'>
---
+++ src/Hours.jsx
@@ -0,0 +1,39 @@
+const Hours = () => {
+  return ( 
+    <div>
+        <h2>Hours of Operation:</h2>
+        <table>
+          <tr>
+            <td>Monday</td>
+            <td>Closed</td>
+          </tr>
+          <tr>
+            <td>Tuesday</td>
+            <td>9:00AM - 7:00PM</td>
+          </tr>
+          <tr>
+            <td>Wednesday</td>
+            <td>9:00AM - 6:00PM</td>
+          </tr>
+          <tr>
+            <td>Thursday</td>
+            <td>9:00AM - 7:00PM</td>
+          </tr>
+          <tr>
+            <td>Friday</td>
+            <td>9:00AM - 7:00PM</td>
+          </tr>
+          <tr>
+            <td>Saturday</td>
+            <td>9:00AM - 7:00PM</td>
+          </tr>
+          <tr>
+            <td>Sunday</td>
+            <td>Closed</td>
+          </tr>
+        </table>
+      </div>
+   )
+}
+ 
+export default Hours
