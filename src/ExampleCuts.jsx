0b17c120f0b00a529ec077607d4f5e5d63463ed0
Brian4755 <brian.mccune1@yahoo.com>
2023-03-28 09:36:54 -0500

    facebook a tag example cuts

--- src/ExampleCuts.jsx
+++ src/ExampleCuts.jsx
@@ -3,10 +3,21 @@ import './ExampleCuts.css'
 const ExampleCuts = () => {
   return ( 
     <div className="examplecuts">
-      <img className='examplecut' src="./cutexample1.jpg" alt="Example Cut" />
-      <img className='examplecut' src="./cutexample2.jpg" alt="Example Cut" />
-      <img className='examplecut' src="./cutexample3.jpg" alt="Example Cut" />
-      <img className='examplecut' src="./cutexample4.jpg" alt="Example Cut" />
+      <a target='_blank' rel='noreferrer' href="https://www.facebook.com/people/Jacks-Barber-Shop/100057022047131/">
+        <img className='examplecut' src="./cutexample1.jpg" alt="Example Cut" />
+      </a>
+
+      <a target='_blank' rel='noreferrer' href="https://www.facebook.com/people/Jacks-Barber-Shop/100057022047131/">
+        <img className='examplecut' src="./cutexample2.jpg" alt="Example Cut" />
+      </a>
+
+      <a target='_blank' rel='noreferrer' href="https://www.facebook.com/people/Jacks-Barber-Shop/100057022047131/">
+        <img className='examplecut' src="./cutexample3.jpg" alt="Example Cut" />
+      </a>
+
+      <a target='_blank' rel='noreferrer' href="https://www.facebook.com/people/Jacks-Barber-Shop/100057022047131/">
+       <img className='examplecut' src="./cutexample4.jpg" alt="Example Cut" />
+      </a>
     </div>
    )
 }
