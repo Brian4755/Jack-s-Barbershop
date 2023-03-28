bf5c8162a6034c4e960d42545e0b704a0463ba78
Brian4755 <brian.mccune1@yahoo.com>
2023-03-28 00:48:00 -0500

    carousel functionality

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -1,7 +1,23 @@
+import { useCallback, useState, useEffect } from "react";
+
 const Carousel = () => {
+  const [carousel, setCarousel] = useState(1)
+
+  const increment = useCallback(() => {
+    setCarousel(carousel*-1)
+  }, [carousel])
+
+  useEffect(() => {
+    const id = setTimeout(increment, 5000)
+    return () => clearTimeout(id)
+  }, [increment])
+
   return ( 
     <>
-    <h2>This is a Carousel Component</h2>
+    {carousel === 1 ?
+    <img src="./example1.jpg" alt="examplecut1" /> :
+    <img src="./example2.jpg" alt="examplecut2" />
+    }
     </>
    )
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -6,8 +6,6 @@ const Home = () => {
     <>
     <img className='insideshop' src="./insidebarbershop.jpg" alt="Inside Area" />
     <h1>"One of the best haircuts I have ever had. Super friendly and nice atmosphere."</h1>
-    {/* <img src="./example1.jpg" alt="examplecut1" />
-    <img src="./example2.jpg" alt="examplecut2" /> */}
     <Carousel />
     </>
    )
