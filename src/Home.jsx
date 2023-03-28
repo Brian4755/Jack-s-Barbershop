4ac5e3da54aab82932062bcbb24c0030cd2c4e0e
Brian4755 <brian.mccune1@yahoo.com>
2023-03-28 03:16:38 -0500

    hours of operation table homepage

--- src/Home.jsx
+++ src/Home.jsx
@@ -23,6 +23,39 @@ const Home = () => {
       </div>
       <img className='barberself' src="./barberself.jpg" alt="Lead Barber" />
       <img className='barberStation' src="./barberstations.jpg" alt="Barber Stations" />
+      <div>
+        <h2>Hours of Operation:</h2>
+        <table>
+          <tr>
+            <td>Monday</td>
+            <td>Closed</td>
+          </tr>
+          <tr>
+            <td>Tuesday</td>
+            <td>9:00AM - 7:00PM</td>
+          </tr>
+          <tr>
+            <td>Wednesday</td>
+            <td>9:00AM - 6:00PM</td>
+          </tr>
+          <tr>
+            <td>Thursday</td>
+            <td>9:00AM - 7:00PM</td>
+          </tr>
+          <tr>
+            <td>Friday</td>
+            <td>9:00AM - 7:00PM</td>
+          </tr>
+          <tr>
+            <td>Saturday</td>
+            <td>9:00AM - 7:00PM</td>
+          </tr>
+          <tr>
+            <td>Sunday</td>
+            <td>Closed</td>
+          </tr>
+        </table>
+      </div>
     </div>
     </>
    )
