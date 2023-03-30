d996035e544e2e8d14aedfb264b6116e7ba2d1c1
Brian4755 <brian.mccune1@yahoo.com>
2023-03-30 10:46:56 -0500

    about text change and position

--- src/About.jsx
+++ src/About.jsx
@@ -5,8 +5,8 @@ const About = () => {
     <div className='aboutPage'>
       <img className="aboutImage" src="./AboutImage.jpg" alt="Barber Shaving Customer" />
       <div className='aboutUsText'>
-        <h1>About Us</h1>
-        <p>We are a proud, family owned barbershop in Manor and first opened in 2018. We have been working under smaller businesses and finally decided to open our own. Still in 2023, we are open and have been cutting hair for customers surrounding Austin.</p>
+        <h1 className='aboutUsTitle'>Where We Started</h1>
+        <p className='aboutUsDetails'>We are a proud, family owned barbershop in Manor and first opened in 2018. We have been working under smaller businesses and finally decided to open our own. Still in 2023, we are open and have been cutting hair for customers surrounding Austin.</p>
       </div>
     </div>
    )
