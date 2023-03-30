0352886d0998862bbde1fad3a1c7dcf2b76369d7
Brian4755 <brian.mccune1@yahoo.com>
2023-03-29 22:12:18 -0500

    hours table fix

--- src/Hours.jsx
+++ src/Hours.jsx
@@ -3,34 +3,36 @@ const Hours = () => {
     <div>
         <h2>Hours of Operation:</h2>
         <table>
-          <tr>
-            <td>Monday</td>
-            <td>Closed</td>
-          </tr>
-          <tr>
-            <td>Tuesday</td>
-            <td>9:00AM - 7:00PM</td>
-          </tr>
-          <tr>
-            <td>Wednesday</td>
-            <td>9:00AM - 6:00PM</td>
-          </tr>
-          <tr>
-            <td>Thursday</td>
-            <td>9:00AM - 7:00PM</td>
-          </tr>
-          <tr>
-            <td>Friday</td>
-            <td>9:00AM - 7:00PM</td>
-          </tr>
-          <tr>
-            <td>Saturday</td>
-            <td>9:00AM - 7:00PM</td>
-          </tr>
-          <tr>
-            <td>Sunday</td>
-            <td>Closed</td>
-          </tr>
+          <tbody>
+            <tr>
+              <td>Monday</td>
+              <td>Closed</td>
+            </tr>
+            <tr>
+              <td>Tuesday</td>
+              <td>9:00AM - 7:00PM</td>
+            </tr>
+            <tr>
+              <td>Wednesday</td>
+              <td>9:00AM - 6:00PM</td>
+            </tr>
+            <tr>
+              <td>Thursday</td>
+              <td>9:00AM - 7:00PM</td>
+            </tr>
+            <tr>
+              <td>Friday</td>
+              <td>9:00AM - 7:00PM</td>
+            </tr>
+            <tr>
+              <td>Saturday</td>
+              <td>9:00AM - 7:00PM</td>
+            </tr>
+            <tr>
+              <td>Sunday</td>
+              <td>Closed</td>
+            </tr>
+          </tbody>
         </table>
       </div>
    )
