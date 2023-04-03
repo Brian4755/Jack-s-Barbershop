e64d448671016a6279385a5338a2e0633401057d
Brian4755 <brian.mccune1@yahoo.com>
2023-04-03 07:47:08 -0500

    about me tect change

--- src/About.jsx
+++ src/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
       <img className="aboutImage" src="./AboutImage.jpg" alt="Barber Shaving Customer" />
       <div className='aboutUsText'>
         <h1 className='aboutUsTitle'>Where We Started</h1>
-        <p className='aboutUsDetails'>We are a proud, family owned barbershop in Manor and first opened in 2018. We have been working under smaller businesses and finally decided to open our own. Still in 2023, we are open and have been cutting hair for customers surrounding Austin.</p>
+        <p className='aboutUsDetails'>We are a proud, family owned barbershop in Manor and first opened in 2018. With over 15 years of experience, we finally decided to open our own business. Still in 2023, we are open and have been cutting hair for customers surrounding Austin.</p>
       </div>
     </div>
    )
